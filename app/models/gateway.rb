97a3881de80041f4f49c6bd95fa158e0fc7222bd
Roman Snitko <roman.snitko@gmail.com>
2015-02-19 16:20:40 +0700

    Gateway#exchange_rate_adapter_names and serialization

--- app/models/gateway.rb
+++ app/models/gateway.rb
@@ -4,12 +4,33 @@ class Gateway < ActiveRecord::Base
 
   belongs_to :user
   serialize :db_config, Hash
+  serialize :exchange_rate_adapter_names
 
   validates :confirmations_required, :pubkey, :name,
             :default_currency, :check_signature, :user,
             presence: true
 
-  validates :confirmations_requires, numericality: { greater_than_or_equal_to: 0 }
-  # validate Adapter exchange_rate_adapter_names are from the available list
+  validates :confirmations_required, numericality: { greater_than_or_equal_to: 0 }
 
+  before_validation :split_exchange_rate_adapter_names!
+  validate          :validate_exchange_rate_adapter_names, if: 'self.exchange_rate_adapter_names.present?'
+
+  private
+
+    def validate_exchange_rate_adapter_names
+      self.exchange_rate_adapter_names = self.exchange_rate_adapter_names.map do |a|
+        begin
+          Kernel.const_get("Straight::ExchangeRate::#{a}Adapter")
+        rescue
+          errors.add(:exchange_rate_adapter_names, "includes adapter #{a}Adapter which is not available")
+        end
+      end
+    end
+
+    def split_exchange_rate_adapter_names!
+      if self.exchange_rate_adapter_names.kind_of?(String)
+        self.exchange_rate_adapter_names = self.exchange_rate_adapter_names.split(/,\s*/)
+      end
+    end
+  
 end
--- spec/models/gateway_spec.rb
+++ spec/models/gateway_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Gateway, type: :model do
+
+  describe "validations" do
+
+    before(:each) do
+      @gateway = build(:gateway)
+    end
+
+    it "validates exchange_rate_adapter_names are actual available classes" do
+      @gateway.exchange_rate_adapter_names = "Bitpay, Coinbase, Btce"
+      expect(@gateway.valid?).to be_truthy
+      @gateway.exchange_rate_adapter_names = "Bitpay, Coinbase, Nonexistent"
+      expect(@gateway.valid?).to be_falsy
+      @gateway.exchange_rate_adapter_names = ""
+      expect(@gateway.valid?).to be_truthy
+    end
+
+  end
+
 end
