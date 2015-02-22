a4389dbb163e1931006e2442ac8490129fc2da39
Roman Snitko <roman.snitko@gmail.com>
2015-02-22 17:55:55 +0700

    Two basic layouts (unauthenticated and application) and a method in AppController to choose one of them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_unauthenticated_layout
+
+  @@layouts = YAML.load_file("#{Rails.root}/config/layouts.yml")
+
+  private
+
+    # Chooses the right layout based on the config/layouts.yml file
+    # which is NOT EXCLUDED from the version control.
+    # This method is needed to avoid manually stating what layout we need in
+    # each controller and action, espcially because we don't want to
+    # generate Devise controllers.
+    def set_unauthenticated_layout
+      @@layouts.each do |name, controllers|
+        controllers.each do |_controller, actions|
+
+          if params[:controller] == _controller
+            actions.each do |_action|
+              if params[:action] == _action
+                self.class.layout name.to_s
+                return
+              end
+            end
+          end
+
+        end
+      end
+    end
 
     def render_403
       respond_to do |format|
