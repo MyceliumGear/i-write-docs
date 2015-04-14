0314f1691b63b8794c401a4d2d12c5d35f2845f0
Roman Snitko <roman.snitko@gmail.com>
2015-04-15 03:24:32 +0700

    Set mailer config options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = {
-    host: 'admin.gearpayments.com'
+    host: 'admin.gear.mycelium.com'
   }
 
 end
