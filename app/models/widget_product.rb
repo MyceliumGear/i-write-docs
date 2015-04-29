9452eb2e0248a668645837b3260f776c58800873
Roman Snitko <roman.snitko@gmail.com>
2015-04-29 23:08:48 +0700

    Widget product validations in the backend and the frontend

--- app/models/widget_product.rb
+++ app/models/widget_product.rb
@@ -5,5 +5,6 @@ class WidgetProduct < ActiveRecord::Base
   validates :title, :price, presence: true
   validates :title, length: { minimum: 2, maximum: 40 }
   validates :price, numericality: { greater_than: 0 }
+  validates :title, format: { with: /\A[^"'<>]+\z/, message: "cannot contain special chatacters such as \", ' and <>"}
 
 end
