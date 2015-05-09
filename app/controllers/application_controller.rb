1b80d80f5a57bf7cc254ad92c1add9d53abc4734
Roman Snitko <roman.snitko@gmail.com>
2015-05-10 00:02:57 +0700

    Fix: Devise prevented User#name from being saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_unauthenticated_layout
   before_filter :prepare_menu
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :render_to_string
 
@@ -62,6 +63,11 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       request.env['omniauth.origin'] || stored_location_for(resource) || '/wizard'
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up)        << :name
+      devise_parameter_sanitizer.for(:account_update) << :name
     end
 
 end
