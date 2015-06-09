a5541a5c65c4e2d5b9b62984d46d76475884b1ea
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2015-06-09 09:39:41 +0300

    Fix: payment notifications mailer spec

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -13,6 +13,7 @@ FactoryGirl.define do
     sequence(:id)          { |i| i }
     sequence(:keychain_id) { |i| i }
     sequence(:address)     { |i| "address_#{i}" }
+    sequence(:payment_id)  { |i| "payment_#{i}" }
     amount 1
     to_create { |order| order.save }
   end
