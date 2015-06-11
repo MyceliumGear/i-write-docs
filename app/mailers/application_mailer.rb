2e40aa0aa77dcf850e8c03a4369f252a7831c2ee
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2015-06-11 13:45:45 +0300

    Fix: email style

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
+  include Roadie::Rails::Automatic
+
   default from: Rails.application.secrets.mailer_sender
   layout 'email'
+
+  private def roadie_options
+    super unless Rails.env.test?
+  end
 end
