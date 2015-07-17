c6c19146419b869f9ba3121d5feef54c1b9b1508
Alex Gornov <zmokizmoghi@gmail.com>
2015-07-17 17:20:15 +0700

    Fix: Rewrite theme != migration

--- db/migrate/20150717075054_widget_theme_cannot_be_null.rb
+++ db/migrate/20150717075054_widget_theme_cannot_be_null.rb
@@ -1,8 +1,6 @@
 class WidgetThemeCannotBeNull < ActiveRecord::Migration
   def change
+    Widget.where(theme: nil).update_all(theme: 'Gray')
     change_column :widgets, :theme, :string, default: 'Gray', null: false
-    Widget.all.each do |widget|
-      widget.update_attributes!(theme:'Gray') if widget.theme.nil?
-    end
   end
 end
