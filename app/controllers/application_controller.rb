a3078e8359d9f96ad07cda2e9a3c4f6923facde4
Alex Gornov <zmokizmoghi@gmail.com>
2015-08-17 17:47:16 +0800

    Fix: Set locale before menu generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
   before_filter :set_unauthenticated_layout
   before_filter :prepare_menu
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_filter :set_locale
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
