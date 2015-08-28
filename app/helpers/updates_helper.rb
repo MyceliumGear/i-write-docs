a53567291ce38ee7e91b16d93723a46effd371c6
Alex Gornov <zmokizmoghi@gmail.com>
2015-08-28 16:05:27 +0800

    Fixes after CR

--- app/helpers/updates_helper.rb
+++ app/helpers/updates_helper.rb
@@ -1,12 +1,12 @@
 module UpdatesHelper
   def priority_indicator(item)
     case item.priority
-      when 'regular'
-        return
-      when 'important'
-        return 'orange'
-      when 'critical'
-        return 'red'
+    when 'regular'
+      return
+    when 'important'
+      return 'orange'
+    when 'critical'
+      return 'red'
     end
   end
 end
