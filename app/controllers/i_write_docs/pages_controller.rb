ce3149eec7bb1b714b1adf4b8875c8ac9c892958
Dmitry Tymchuk <dsnipe@gmail.com>
2015-10-07 16:40:21 +0300

    Catch some errors in controller

--- app/controllers/i_write_docs/pages_controller.rb
+++ app/controllers/i_write_docs/pages_controller.rb
@@ -4,16 +4,16 @@ module IWriteDocs
     def show
       page = params[:doc] || 'index'
       @node = IWriteDocs.docs_tree.find_node_by_url(page)
-      raise ActionController::RoutingError.new('Not found') unless @node
-      @content = prepare_page_content(@node.content[:source_path], session[:version])
-      render 'body', locals: { content: @content }
+      raise ActionController::RoutingError.new('Not found') if node_is_not_document?(@node) 
+      content = prepare_page_content(@node.content[:source_path], session[:version])
+      render 'body', locals: { content: content }
     end
 
     def diff
       file = params[:file]
       @tag = params[:tag]
-      redirect_to(iwd.root_path) and return if file.to_s.empty? || @tag.to_s.empty?
       @node = IWriteDocs.docs_tree.find_node_by_url(file)
+      raise ActionController::RoutingError.new('Not found') if @tag.to_s.empty? || node_is_not_document?(@node)
       file_path = @node.content[:source_path] +'.md'
       html_content = Differ.new(file_path, @tag, session[:version]).result.html_safe
       render 'body', locals: { content: html_content }
@@ -25,6 +25,10 @@ module IWriteDocs
     end
 
   private
+
+    def node_is_not_document?(node)
+      node.nil? || node.has_children?
+    end
     
     def prepare_page_content(path, ver)
       source = IWriteDocs.repo.get_file_content(path+ '.md', ver)
--- test/differ_test.rb
+++ test/differ_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 describe IWriteDocs::Differ do
   it "returns diff of specific file" do
-    IWriteDocs::Differ.new('source/getting_started.md', "v1", '').must_equal ""
+    expected = "<div class='diff'><div class='del'><span class='symbol'>-</span><h1>Getting Started</h1></div><div class='ins'><span class='symbol'>+</span><h2>Getting Started</h2></div><div class='ins'><span class='symbol'>+</span></div><div class='ins'><span class='symbol'>+</span><p>Content with <em>italic</em> text</p></div><div class='ins'><span class='symbol'>+</span></div><div class='ins'><span class='symbol'>+</span><p>List:</div><div class='ins'><span class='symbol'>+</span>- one</div><div class='ins'><span class='symbol'>+</span>- two</p></div></div>"
+    IWriteDocs::Differ.new('source/getting_started.md', "v1", '').result.must_equal expected
   end
 end
