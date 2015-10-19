b068eb3cd2757b93612534c49e767ccefa36be94
Dmitry Tymchuk <dsnipe@gmail.com>
2015-10-19 12:37:41 +0300

    Fix diff render problem and add no diff text

--- lib/i-write-docs/differ.rb
+++ lib/i-write-docs/differ.rb
@@ -6,14 +6,20 @@ module IWriteDocs
     def initialize(file_path, target_version, current_version)
       @target_content  = IWriteDocs.repo.get_file_content(file_path, target_version).force_encoding(Encoding::UTF_8)
       @current_content = IWriteDocs.repo.get_file_content(file_path, current_version).force_encoding(Encoding::UTF_8)
-      make_diff
+      @result = make_diff
     end
 
   private
 
     def make_diff
-      prepared_diff = "```diff\n"+ Diffy::Diff.new(@target_content, @current_content).to_s(:text) +"\n```\n"
-      @result = MarkdownRender.parse_to_html(prepared_diff)
+      diff = Diffy::Diff.new(@target_content, @current_content).to_s(:text)
+      return "<p class='noDiff'>#{I18n.t('i-write-docs.no-diff')}</p>" if diff.empty?
+      prepared_diff = "```diff\n" + remove_special_chars(diff) + "\n```\n"
+      MarkdownRender.parse_to_html(prepared_diff)
+    end
+
+    def remove_special_chars(content)
+      content.gsub(/```/, "'''")
     end
 
   end
